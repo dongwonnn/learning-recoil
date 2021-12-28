9be67c5efbf61959f3a7ded2da02335e5111e5f3
dongwonkim <kkum9408@gmail.com>
2021-12-28 18:26:38 +0900

    wrong commit1

--- src/components/CharacterCounter/index.js
+++ src/components/CharacterCounter/index.js
@@ -15,7 +15,7 @@ const CharacterCounter = () => {
             <div>
                 <input type="text" value={text} onChange={onChange} />
                 <br />
-                Echo: {text}
+                Echo: {text} 22로 바꿀 예정
             </div>
             <>Character Count: {count}</>
         </div>
